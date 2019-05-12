5eb8ffff6aa97a1350ca34c8877523026fee31e5
Laura <LG3295@gmail.com>
2019-05-12 02:36:22 -0700

    initial setup

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.send('public/index.html'))
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
