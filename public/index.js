cf323bf7e4eb506f3b9682b8b0bb174b03a1bdb2
Laura <LG3295@gmail.com>
2019-07-03 09:57:12 -0700

    navbarbackground, social media icons

---
+++ public/index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var jumboElement = document.getElementById("jumbo");
+
+setTimeout(function() {
+  var navEl = document.getElementById("navbar");
+  navEl.classList.add("blackbackground");
+}, 3000);
+
+
+function toggleNavBackgroundColor() {
+    var navElement = document.getElementsByTagName("nav")[0];
+    alert(navElement.style.backgroundColor);
+    if (navElement.style.backgroundColor !== "black")
+    {
+      navElement.style.backgroundColor == "transparent";
+    }
+    else {
+      navElement.style.backgroundColor == "black";
+    }
+}
